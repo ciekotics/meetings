587e0a03d4737653076e3a7a7e5b4987e62ec4c7
cieko <rahulsaxena2021@hotmail.com>
2024-04-01 05:33:07 +0530

    changes

--- app/(root)/(home)/personal-room/page.tsx
+++ app/(root)/(home)/personal-room/page.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-// import { useUser } from "@clerk/nextjs";
-// import { useStreamVideoClient } from "@stream-io/video-react-sdk";
+import { useUser } from "@clerk/nextjs";
+import { useStreamVideoClient } from "@stream-io/video-react-sdk";
 import { useRouter } from "next/navigation";
 
-// import { useGetCallById } from "@/hooks/useGetCallById";
+import { useGetCallById } from "@/hooks/useGetCallById";
 import { Button } from "@/components/ui/button";
 import { useToast } from "@/components/ui/use-toast";
 
@@ -29,51 +29,51 @@ const Table = ({
 
 const PersonalRoom = () => {
   const router = useRouter();
-  // const { user } = useUser();
-  // const client = useStreamVideoClient();
+  const { user } = useUser();
+  const client = useStreamVideoClient();
   const { toast } = useToast();
 
-  // const meetingId = user?.id;
+  const meetingId = user?.id;
 
-  // const { call } = useGetCallById(meetingId!);
+  const { call } = useGetCallById(meetingId!);
 
-  // const startRoom = async () => {
-  //   if (!client || !user) return;
+  const startRoom = async () => {
+    if (!client || !user) return;
 
-  //   const newCall = client.call("default", meetingId!);
+    const newCall = client.call("default", meetingId!);
 
-  //   if (!call) {
-  //     await newCall.getOrCreate({
-  //       data: {
-  //         starts_at: new Date().toISOString(),
-  //       },
-  //     });
-  //   }
+    if (!call) {
+      await newCall.getOrCreate({
+        data: {
+          starts_at: new Date().toISOString(),
+        },
+      });
+    }
 
-  //   router.push(`/meeting/${meetingId}?personal=true`);
-  // };
+    router.push(`/meeting/${meetingId}?personal=true`);
+  };
 
-  // const meetingLink = `${process.env.NEXT_PUBLIC_BASE_URL}/meeting/${meetingId}?personal=true`;
+  const meetingLink = `${process.env.NEXT_PUBLIC_BASE_URL}/meeting/${meetingId}?personal=true`;
 
   return (
     <section className="flex size-full flex-col gap-10 text-white">
       <h1 className="text-xl font-bold lg:text-3xl">Personal Meeting Room</h1>
       <div className="flex w-full flex-col gap-8 xl:max-w-[900px]">
-        {/* <Table title="Topic" description={`${user?.username}'s Meeting Room`} /> */}
-        {/* <Table title="Meeting ID" description={meetingId!} /> */}
-        {/* <Table title="Invite Link" description={meetingLink} /> */}
+        <Table title="Topic" description={`${user?.username}'s Meeting Room`} />
+        <Table title="Meeting ID" description={meetingId!} />
+        <Table title="Invite Link" description={meetingLink} />
       </div>
       <div className="flex gap-5">
         <Button 
           className="bg-blue-1" 
-          // onClick={startRoom}
+          onClick={startRoom}
           >
           Start Meeting
         </Button>
         <Button
           className="bg-dark-3"
           onClick={() => {
-            // navigator.clipboard.writeText(meetingLink);
+            navigator.clipboard.writeText(meetingLink);
             toast({
               title: "Link Copied",
             });
